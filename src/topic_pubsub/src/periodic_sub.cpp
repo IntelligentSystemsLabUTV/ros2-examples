8cdc1a33233aa7e74277658f192a1712002b9d33
Roberto Masocco <robmasocco@gmail.com>
2022-01-15 20:31:06 +0100

    feat: add example about topic de(/re)subscription
    @fabrizioromanelli this was the last thing that I needed to address: how to unsubscribe from a topic,
    and possibly resume that subscription later on, something that in ROS 1 is possible with the
    shutdown() method from my understanding. There's no such method in ROS 2, thus it seems that
    topic subscriptions must always stay active (with all their unnecessary processing overhead).
    But the solution is very simple and comes from RViz source code (see ref. in code):
    since subscribers and publishers are handled via shared_ptrs, just reset those so that destructors
    get called, which will clean stuff down to the DDS endpoint layer. Objects can then be recreated
    to resume communication.

---
+++ src/topic_pubsub/src/periodic_sub.cpp
@@ -0,0 +1,71 @@
+/**
+ * Dynamic subscriber example.
+ *
+ * Roberto Masocco <robmasocco@gmail.com>
+ *
+ * January 15, 2022
+ */
+
+#include <chrono>
+
+#include <rclcpp/rclcpp.hpp>
+#include <std_msgs/msg/string.hpp>
+
+using namespace std_msgs::msg;
+using namespace std::chrono_literals;
+
+/**
+ * Subscriber node that dynamically resubscribes to a given topic.
+ * Run this alongside this package's publisher.
+ */
+//! For the sake of simplicity we'll write everything in here this time
+class PeriodicSub : public rclcpp::Node
+{
+public:
+  PeriodicSub()
+  : Node("periodic_subscriber")
+  {
+    //! We just have to initialize the timer
+    sub_timer_ = this->create_wall_timer(
+      5s,
+      [this]() -> void {
+        //! This just has to toggle topic subscription
+        if (sub_) { //! Operator checks if it is nullptr
+          RCLCPP_WARN(this->get_logger(), "De-subscribing from topic");
+          //! Resetting the shared_ptr will make the Subscription fall out of
+          //! scope, thus being destroyed
+          //! ref.: https://answers.ros.org/question/354792/rclcpp-how-to-unsubscribe-from-a-topic/
+          sub_.reset();
+        } else {
+          RCLCPP_WARN(this->get_logger(), "Re-subscribing to topic");
+          //! We just create a new Subscription
+          sub_ = this->create_subscription<String>(
+            "/examples/test_topic",
+            rclcpp::QoS(10),
+            [this](String::SharedPtr msg) -> void {
+              RCLCPP_INFO(this->get_logger(), msg->data.c_str());
+            }
+          );
+          //! And that's it. The same could be done with a Publisher,
+          //! but being it asynchronous by default it's usually less important
+        }
+      }
+    );
+  
+    RCLCPP_INFO(this->get_logger(), "Node initialized");
+  }
+
+private:
+  //! Note that this, apart from the type wrapping, is a classic shared_ptr
+  rclcpp::Subscription<String>::SharedPtr sub_;
+
+  rclcpp::TimerBase::SharedPtr sub_timer_;
+};
+
+int main(int argc, char ** argv) {
+  rclcpp::init(argc, argv);
+  auto sub_node = std::make_shared<PeriodicSub>();
+  rclcpp::spin(sub_node);
+  rclcpp::shutdown();
+  exit(EXIT_SUCCESS);
+}
