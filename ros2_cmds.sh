67ed3f59606450aa56bb3f125dcf19f2d09d9e5d
Roberto Masocco <robmasocco@gmail.com>
2022-05-02 15:29:21 +0200

    feat: remove foxy branch

--- ros2_cmds.sh
+++ ros2_cmds.sh
@@ -31,34 +31,6 @@ ros2galactic() {
   fi
 }
 
-# Initialize ROS 2 Foxy Fitzroy according to the shell.
-# @param domain Domain ID to set initially; can be omitted.
-ros2foxy() {
-  if [[ $# -ne 0 ]]; then
-    export ROS_DOMAIN_ID=$1
-  fi
-  export ROS_VERSION=2
-  export ROS_PYTHON_VERSION=3
-  # Check that the distribution is installed
-  if [[ ! -d /opt/ros/foxy ]]; then
-    >&2 echo "No such ROS 2 distribution installed!"
-    return 1
-  fi
-  export ROS_DISTRO=foxy
-  # Source the ROS 2 installation according to the current shell
-  if [[ -n "$(echo $SHELL | grep 'bash')" ]]; then
-    source /opt/ros/$ROS_DISTRO/setup.bash
-    source /usr/share/colcon_argcomplete/hook/colcon-argcomplete.bash
-  elif [[ -n "$(echo $SHELL | grep 'zsh')" ]]; then
-    source /opt/ros/$ROS_DISTRO/setup.zsh
-    source /usr/share/colcon_argcomplete/hook/colcon-argcomplete.zsh
-    eval "$(register-python-argcomplete3 ros2)"
-    # echo "Remember to source install/local_setup.zsh for your packages!"
-  else
-    source /opt/ros/$ROS_DISTRO/setup.sh
-  fi
-}
-
 # Initializes a ROS 2 workspace; must be ran inside it.
 ros2wsinit() {
   if [[ -z "$ROS_DISTRO" ]]; then
