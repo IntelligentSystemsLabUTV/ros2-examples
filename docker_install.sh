8435b566a31e1dd199e8bafd2bc38f3f48b09bf2
Alessandro Tenaglia <alessandro.tenaglia42@gmail.com>
2022-12-07 16:00:50 +0100

    fix: distribution detection command

--- docker_install.sh
+++ docker_install.sh
@@ -40,8 +40,8 @@ sudo apt-get install -y \
 
 # Add Docker's official GPG key and stable repository
 echo "Setting up Docker's official repository..."
-curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo gpg --dearmor -o /usr/share/keyrings/docker-archive-keyring.gpg
-echo "deb [arch=$(dpkg --print-architecture) signed-by=/usr/share/keyrings/docker-archive-keyring.gpg] https://download.docker.com/linux/ubuntu $(lsb_release -cs) stable" | sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
+curl -fsSL https://download.docker.com/linux/$(. /etc/os-release; echo $ID)/gpg | sudo gpg --dearmor -o /usr/share/keyrings/docker-archive-keyring.gpg
+echo "deb [arch=$(dpkg --print-architecture) signed-by=/usr/share/keyrings/docker-archive-keyring.gpg] https://download.docker.com/linux/$(. /etc/os-release; echo $ID) $(lsb_release -cs) stable" | sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
 sudo apt-get update
 
 # Install Docker Engine
