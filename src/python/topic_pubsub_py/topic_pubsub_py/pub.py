bcd9ad843680a3a7e14e6af394a1a76822472522
Roberto Masocco <robmasocco@gmail.com>
2023-05-22 11:20:19 +0200

    fix: termination, gitignore

--- src/python/topic_pubsub_py/topic_pubsub_py/pub.py
+++ src/python/topic_pubsub_py/topic_pubsub_py/pub.py
@@ -7,8 +7,6 @@ Intelligent Systems Lab <isl.torvergata@gmail.com>
 May 22, 2023
 """
 
-import sys
-
 #! Import Python client library for ROS 2
 import rclpy
 from rclpy.node import Node
@@ -20,40 +18,42 @@ import rclpy.qos
 from ros2_examples_interfaces.msg import String
 
 #! To define a node, we need to inherit from the Node class
+
+
 class Pub(Node):
     """Example ROS 2 node with a subscriber to a custom topic."""
 
     def __init__(self) -> None:
-      """
-      Creates a new Sub node.
-      """
-      #! We need to call the base Node constructor
-      super().__init__('publisher_node')
+        """
+        Creates a new Sub node.
+        """
+        #! We need to call the base Node constructor
+        super().__init__('publisher_node')
 
-      #! Create a custom, best-effort QoS profile
-      qos_profile = rclpy.qos.QoSProfile(
-          depth=1,
-          history=rclpy.qos.HistoryPolicy.KEEP_LAST,
-          reliability=rclpy.qos.ReliabilityPolicy.BEST_EFFORT,
-          durability=rclpy.qos.DurabilityPolicy.VOLATILE)
+        #! Create a custom, best-effort QoS profile
+        qos_profile = rclpy.qos.QoSProfile(
+            depth=1,
+            history=rclpy.qos.HistoryPolicy.KEEP_LAST,
+            reliability=rclpy.qos.ReliabilityPolicy.BEST_EFFORT,
+            durability=rclpy.qos.DurabilityPolicy.VOLATILE)
 
-      #! Create a publisher to the custom topic
-      #! Note that this is Python: we are defining the publiser attribute as we
-      #! create it
-      self._pub = self.create_publisher(
-          String,
-          '~/examples/test_topic',
-          qos_profile)
+        #! Create a publisher to the custom topic
+        #! Note that this is Python: we are defining the publiser attribute as we
+        #! create it
+        self._pub = self.create_publisher(
+            String,
+            '~/examples/test_topic',
+            qos_profile)
 
-      #! Create a timer to publish a message every second
-      self._timer = self.create_timer(
-          0.5,
-          self.timer_callback)
+        #! Create a timer to publish a message every second
+        self._timer = self.create_timer(
+            0.5,
+            self.timer_callback)
 
-      # This will be used to fill messages
-      self._cnt = 0
+        # This will be used to fill messages
+        self._cnt = 0
 
-      self.get_logger().info('Node initialized')
+        self.get_logger().info('Node initialized')
 
     #! Callback functions are methods here too, and are passed message objects
     def timer_callback(self) -> None:
@@ -68,9 +68,10 @@ class Pub(Node):
 
         self.get_logger().info('Published message: %s' % msg.data)
 
-def main():
+
+def main(args=None):
     #! Initialize the ROS 2 client library and context
-    rclpy.init(args=sys.argv)
+    rclpy.init(args=args)
 
     #! Initialize ROS 2 node
     node = Pub()
@@ -79,8 +80,13 @@ def main():
     try:
         rclpy.spin(node)
     except:
-        rclpy.shutdown()
-        print("ops")
+        pass
+        #! If you press Ctrl-C, the handler will call rclpy.shutdown() for us
+    node.destroy_node()
+    # rclpy.shutdown()
+
+    print('Publisher terminated')
+
 
 #! This is required in Python to execute a script
 if __name__ == '__main__':
--- src/python/topic_pubsub_py/topic_pubsub_py/sub.py
+++ src/python/topic_pubsub_py/topic_pubsub_py/sub.py
@@ -20,33 +20,35 @@ import rclpy.qos
 from ros2_examples_interfaces.msg import String
 
 #! To define a node, we need to inherit from the Node class
+
+
 class Sub(rclpy.node.Node):
     """Example ROS 2 node with a subscriber to a custom topic."""
 
     def __init__(self) -> None:
-      """
-      Creates a new Sub node.
-      """
-      #! We need to call the base Node constructor
-      super().__init__('subscriber_node')
+        """
+        Creates a new Sub node.
+        """
+        #! We need to call the base Node constructor
+        super().__init__('subscriber_node')
 
-      #! Create a custom, best-effort QoS profile
-      qos_profile = rclpy.qos.QoSProfile(
-          depth=1,
-          history=rclpy.qos.HistoryPolicy.KEEP_LAST,
-          reliability=rclpy.qos.ReliabilityPolicy.BEST_EFFORT,
-          durability=rclpy.qos.DurabilityPolicy.VOLATILE)
+        #! Create a custom, best-effort QoS profile
+        qos_profile = rclpy.qos.QoSProfile(
+            depth=1,
+            history=rclpy.qos.HistoryPolicy.KEEP_LAST,
+            reliability=rclpy.qos.ReliabilityPolicy.BEST_EFFORT,
+            durability=rclpy.qos.DurabilityPolicy.VOLATILE)
 
-      #! Create a subscriber to the custom topic
-      #! Note that this is Python: we are defining the subscriber attribute as we
-      #! create it
-      self._sub = self.create_subscription(
-          String,
-          '/publisher_node/examples/test_topic',
-          self.sub_callback,
-          qos_profile)
+        #! Create a subscriber to the custom topic
+        #! Note that this is Python: we are defining the subscriber attribute as we
+        #! create it
+        self._sub = self.create_subscription(
+            String,
+            '/publisher_node/examples/test_topic',
+            self.sub_callback,
+            qos_profile)
 
-      self.get_logger().info('Node initialized')
+        self.get_logger().info('Node initialized')
 
     #! Callback functions are methods here too, and are passed message objects
     def sub_callback(self, msg: String) -> None:
@@ -56,6 +58,7 @@ class Sub(rclpy.node.Node):
         :param msg: String message to parse.
         """
         self.get_logger().info('Received message: %s' % msg.data)
+
 
 def main():
     #! Initialize the ROS 2 client library and context
@@ -68,7 +71,13 @@ def main():
     try:
         rclpy.spin(node)
     except:
-        rclpy.shutdown()
+        pass
+        #! If you press Ctrl-C, the handler will call rclpy.shutdown() for us
+    node.destroy_node()
+    # rclpy.shutdown()
+
+    print('Subscriber terminated')
+
 
 #! This is required in Python to execute a script
 if __name__ == '__main__':
