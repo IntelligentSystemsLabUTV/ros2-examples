a8763b6f2bede042387accc17f6b3fb1fb01aa84
Roberto Masocco <robmasocco@gmail.com>
2023-05-30 15:48:32 +0200

    fix: pip3 installation

--- bin/ros2_humble_install.sh
+++ bin/ros2_humble_install.sh
@@ -23,7 +23,7 @@ if ! apt-cache policy | grep -q 'universe'; then
 fi
 
 echo "Adding ROS 2 apt repository..."
-sudo apt update && sudo apt install -y curl gnupg lsb-release
+sudo apt update && sudo apt install -y curl gnupg lsb-release python3 python3-pip
 sudo curl -sSL https://raw.githubusercontent.com/ros/rosdistro/master/ros.key  -o /usr/share/keyrings/ros-archive-keyring.gpg
 echo "deb [arch=$(dpkg --print-architecture) signed-by=/usr/share/keyrings/ros-archive-keyring.gpg] http://packages.ros.org/ros2/ubuntu $(lsb_release -cs) main" | sudo tee /etc/apt/sources.list.d/ros2.list > /dev/null
 sudo apt update
